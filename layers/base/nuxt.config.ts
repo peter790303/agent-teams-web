3be02f041214cb4ce2fa3a75a70b2eac0bd5c695
peter <peter790303@gmail.com>
2026-09-15 12:37:53 +0800

    fix(web): bundle Vuetify for SSR

--- layers/base/nuxt.config.ts
+++ layers/base/nuxt.config.ts
@@ -3,5 +3,8 @@ import vuetify from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
   vite: {
     plugins: [vuetify({ autoImport: true })],
+    ssr: {
+      noExternal: ['vuetify'],
+    },
   },
 })
